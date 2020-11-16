9822f65031efe5bf880243f0c2babd9864bc03f6
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2020-11-16 03:30:17 +0100

    Add files via upload

--- AmpliCraft/src/ampliCraft/MysteryBox.java
+++ AmpliCraft/src/ampliCraft/MysteryBox.java
@@ -1,24 +1,26 @@
 package ampliCraft;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 public class MysteryBox {
 	Player p;
 	AmpliCraft plugin;
+	HashMap<Integer, ItemStack> itemList = new HashMap<Integer, ItemStack>();
 	public MysteryBox(Player p, AmpliCraft plugin) {
 		this.p = p;
 		this.plugin = plugin;
+		initItemList();
 	}
 	public void openMysteryBox() {
 		p.getWorld().spawnParticle(Particle.SPELL, p.getLocation(), 100);
@@ -26,21 +28,34 @@ public class MysteryBox {
 		p.getWorld().spawnParticle(Particle.DRIP_LAVA, p.getLocation(), 100);
 		p.getWorld().playEffect(p.getLocation(), Effect.SMOKE, 0);
 		p.getWorld().playEffect(p.getLocation(), Effect.INSTANT_POTION_BREAK, 0);
-		ItemStack LeatherHelmet1 = new ItemStack(Material.LEATHER_HELMET); 
-		LeatherArmorMeta meta = (LeatherArmorMeta) LeatherHelmet1.getItemMeta();
-		meta.setColor(Color.PURPLE);
-		LeatherHelmet1.setItemMeta(meta);
-
-		
-	    ItemStack item = new ItemStack(Material.DIAMOND_SWORD, 1);
-	    ItemMeta im = item.getItemMeta();
-	    im.setDisplayName(ChatColor.RED + "Nashorum");
-	    List<String> loreList = new ArrayList<String>();
-	    loreList.add(ChatColor.GOLD + "Das legendäre Schwert!");//This is the first line of lore
-	    loreList.add(ChatColor.GOLD + "Es wird erzählt es hätte bereits einen Drachen besiegt");//This is the second line of lore
-	    im.setLore(loreList);
-	    item.setItemMeta(im);
-	    p.getInventory().addItem(item);
-	    //p.getInventory().addItem(LeatherHelmet1);
+		p.getInventory().addItem(randomItem());
+	}
+	private ItemStack randomItem() {
+		int randomNum = ThreadLocalRandom.current().nextInt(0, 4 + 1);
+		return itemList.get(randomNum);
+	}
+	private void initItemList() {
+		//===================================================================
+		ItemStack nashorum = new ItemStack(Material.DIAMOND_SWORD, 1);
+		ItemMeta nashorumIm = nashorum.getItemMeta();
+		nashorumIm.setDisplayName(ChatColor.RED + "Nashorum");
+		List<String> loreList = new ArrayList<String>();
+		loreList.add(ChatColor.GOLD + "Das legendäre Schwert!");//This is the first line of lore
+		loreList.add(ChatColor.GOLD + "Es wird erzählt es hätte bereits einen Drachen besiegt");//This is the second line of lore
+		nashorumIm.setLore(loreList);
+		nashorum.setItemMeta(nashorumIm);
+		itemList.put(0, nashorum);
+		//===================================================================
+		ItemStack diamondBlock = new ItemStack(Material.DIAMOND_BLOCK, 2);
+		itemList.put(1, diamondBlock);
+		//===================================================================
+		ItemStack enderPerl = new ItemStack(Material.ENDER_PEARL, 3);
+		itemList.put(2, enderPerl);
+		//===================================================================
+		ItemStack netherArmor = new ItemStack(Material.NETHERITE_CHESTPLATE, 1);
+		itemList.put(3, netherArmor);
+		//===================================================================
+		ItemStack gold = new ItemStack(Material.GOLD_INGOT, 10);
+		itemList.put(4, gold);
 	}
 }
